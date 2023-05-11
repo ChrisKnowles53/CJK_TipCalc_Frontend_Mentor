c59d0be63b89cc32edfc725cefa76a1942e8e4ac
ChrisKnowles <christianjknowles@gmail.com>
2023-05-11 08:57:39 +0100

    rest logic functioning

--- TipCalc.js
+++ TipCalc.js
@@ -19,12 +19,11 @@ tipButtons.forEach((button) => {
 });
 
 customTipInput.addEventListener("input", () => {
- calculateTipPerPerson(customTipInput.value);
+  calculateTipPerPerson(customTipInput.value);
 });
 
 // Function to calculate the total per person
 function calculateTotalPerPerson() {
-  
   const amount = amountInput.value;
   const numberOfPeople = numberOfPeopleInput.value;
   const totalPerPerson = (amount / numberOfPeople).toFixed(2);
@@ -33,17 +32,31 @@ function calculateTotalPerPerson() {
   totalPerPersonElement.textContent = totalPerPerson;
 }
 
-
 function calculateTipPerPerson(tipButtonValue) {
-  
   const amount = amountInput.value;
   const numberOfPeople = numberOfPeopleInput.value;
   // parseFloat to concert from 5% to 0.05
   let tipPercentage = parseFloat(tipButtonValue) / 100;
 
   const tipAmount = ((amount * tipPercentage) / numberOfPeople).toFixed(2);
-  
+
   // Update the text of the tip per person element
   tipPersonElement.textContent = tipAmount;
- }
- 
+}
+
+const resetButton = document.querySelector(".reset-button");
+
+resetButton.addEventListener("click", resetCalculator);
+
+function resetCalculator() {
+  amountInput.value = "";
+  numberOfPeopleInput.value = 1;
+  customTipInput.value = "";
+  // Reset tip buttons
+  tipButtons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  totalPerPersonElement.textContent = "0.00";
+  tipPersonElement.textContent = "0.00";
+}
